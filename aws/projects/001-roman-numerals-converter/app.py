a9cb81a93c1a376997535d5631bcd6274a45261c
Shawn <shawn.burroughs135@gmail.com>
2023-10-30 19:57:25 -0500

    new data

--- aws/projects/001-roman-numerals-converter/app.py
+++ aws/projects/001-roman-numerals-converter/app.py
@@ -15,15 +15,14 @@ def main_post():
     if request.method == 'POST':
         alpha = request.form['number']
         if not alpha.isdecimal():
-            return render_template('index.html', developer_name='osvaldo', not_valid=True)
+            return render_template('index.html', developer_name='Shawn', not_valid=True)
         number = int(alpha)
         if not 0 < number < 4000:
-            return render_template('index.html', developer_name='osvaldo', not_valid=True)
-        return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='osvaldo')
+            return render_template('index.html', developer_name='Shawn', not_valid=True)
+        return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='Shawn')
     else:
-        return render_template('index.html', developer_name='osvaldo', not_valid=False)
+        return render_template('index.html', developer_name='Shawn', not_valid=False)
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=8080)
-
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
--- aws/projects/001-roman-numerals-converter/userdataRomannum.sh
+++ aws/projects/001-roman-numerals-converter/userdataRomannum.sh
@@ -7,7 +7,7 @@ pip3 install flask
 
 
 
-FOLDER="https://github.com/shawn135/my-repository/tree/main/aws/projects/001-roman-numerals-converter"
+FOLDER="https://raw.githubusercontent.com/shawn135/my-repository/main/aws/projects/001-roman-numerals-converter"
 
 cd /home/ec2/user
 wget ${FOLDER}/app.py
@@ -16,3 +16,5 @@ cd templates
 wget ${FOLDER}/templates/index.html
 wget ${FOLDER}/templates/result.html
 cd ..
+
+python3 app.py
